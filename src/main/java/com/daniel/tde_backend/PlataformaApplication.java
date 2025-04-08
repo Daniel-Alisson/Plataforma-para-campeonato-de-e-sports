11999c199ffa4ba312f8232f03693f4d4c3f8ab8
Daniel Alisson <danielalisson77@gmail.com>
2025-04-08 18:28:05 -0300

    Base do Projeto

---
+++ src/main/java/com/daniel/tde_backend/PlataformaApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.tde_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/tde_backend/PlataformaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.tde_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
