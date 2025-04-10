26b060da875f5ed906ca3511a6b261078b84f044
Daniel Alisson <danielalisson77@gmail.com>
2025-04-09 23:13:36 -0300

    fix: mensagens de validações

--- src/main/java/com/daniel/tde_backend/dto/UsuarioDTO.java
+++ src/main/java/com/daniel/tde_backend/dto/UsuarioDTO.java
@@ -2,8 +2,7 @@ package com.daniel.tde_backend.dto;
 
 import com.daniel.tde_backend.models.Usuario;
 import com.daniel.tde_backend.models.enums.UsuarioStatus;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 
@@ -13,11 +12,19 @@ public class UsuarioDTO {
     @Size(min = 3, max = 50, message = "Nome precisa ter de 3 a 50 caracteres")
     @NotBlank(message = "Campo requerido")
     private String nome;
+    @Size(min = 3, max = 50, message = "Nickname precisa ter de 3 a 50 caracteres")
     private String nickName;
+    @NotBlank(message = "Campo requerido")
+    @Email(message = "Email inválido")
     private String email;
+    @NotBlank(message = "Campo requerido")
+    @Size(min = 8, message = "Senha deve ter no mínimo 8 caracteres")
     private String senha;
+    @NotNull(message = "Campo requerido")
+    @Past(message = "Data precisa ser válida")
     private LocalDate dataNascimento;
     private String imgUrl;
+    @NotNull(message = "Campo requerido")
     private UsuarioStatus status;
 
     public UsuarioDTO() {
--- src/main/java/com/daniel/tde_backend/exceptions/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/daniel/tde_backend/exceptions/handlers/ControllerExceptionHandler.java
@@ -3,9 +3,12 @@ package com.daniel.tde_backend.exceptions.handlers;
 import com.daniel.tde_backend.exceptions.CustomError;
 import com.daniel.tde_backend.exceptions.DatabaseException;
 import com.daniel.tde_backend.exceptions.ResourceNotFoundException;
+import com.daniel.tde_backend.exceptions.ValidationError;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -21,9 +24,20 @@ public class ControllerExceptionHandler {
         return ResponseEntity.status(status).body(err);
     }
 
+    @ExceptionHandler(DatabaseException.class)
     public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(e.getMessage(), Instant.now(), request.getRequestURI(), status.value());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError("Dados inválidos", Instant.now(), request.getRequestURI(), status.value());
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
         return ResponseEntity.status(status).body(err);
     }
 }
